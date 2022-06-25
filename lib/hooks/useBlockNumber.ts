ce3b2e4ac6c85a242091647fe9c98bfe0c1e12fc
iam-dev <elky.bachtiar@duo.nl>
2022-06-25 12:28:08 +0200

    solve Web3Provider for deployment

--- lib/hooks/useBlockNumber.ts
+++ lib/hooks/useBlockNumber.ts
@@ -1,8 +1,9 @@
 import { JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
+import { providers } from "ethers";
 import useSWR from "swr";
 
-function getBlockNumber(library: Web3Provider) {
+function getBlockNumber(library: Web3Provider | any) {
   return async () => {
     return library.getBlockNumber();
   };
@@ -12,18 +13,7 @@ export default function useBlockNumber() {
   const { library } = useWeb3React<Web3Provider>();
   const shouldFetch = !!library;
 
-  let newLibrary: Web3Provider;
-  if (library) {
-    newLibrary = library;
-  } else {
-    newLibrary = new JsonRpcProvider("http://localhost:8545");
-  }
-
-  return useSWR(
-    shouldFetch ? ["BlockNumber"] : null,
-    getBlockNumber(newLibrary),
-    {
-      refreshInterval: 10 * 1000,
-    }
-  );
+  return useSWR(shouldFetch ? ["BlockNumber"] : null, getBlockNumber(library), {
+    refreshInterval: 10 * 1000,
+  });
 }
